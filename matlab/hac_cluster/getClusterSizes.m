0198f46356fdc0a64aa3933f3c13b20c1104967c
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-05-18 00:02:41 +1000

    matlab: Remove all folders

--- matlab/hac_cluster/getClusterSizes.m
+++
@@ -1,5 +0,0 @@
-function sizes = getClusterSizes(clusters)
-
-sizes = arrayfun(@(x)(length(x.mergedPts)), clusters);
-
-end
--- matlab/lgsp_arcs/angleAverage.m
+++
@@ -1,6 +0,0 @@
-function avg = angleAverage(angs)
-% Average of the given input angles (bearings), in radians.
-
-avg = atan2(sum(sin(angs)) / sum(cos(angs)));
-
-end
