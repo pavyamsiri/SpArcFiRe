1232c3022b4fc3598be91cee6df7b883ad7d2f8b
Pavadol Yamsiri <pavyamsiri@gmail.com>
2024-05-18 00:08:33 +1000

    matlab: Remove Nair-Abraham catalogue functions
    These explicitly reference local file paths which doesn't make sense
    to add in the git repo which is supposed to be generic over all users.

--- matlab/LoadNairAbrahamCatalog.m
+++
@@ -1,8 +0,0 @@
-cattbl = readTableFromFile('D:\Galaxy-Sets\NairAbrahamMorphology.cat');
-idtbl = readTableFromFile('D:\Galaxy-Sets\SDSSids.tbl');
-
-idtbl(2:end, 3) = regexprep(idtbl(2:end, 3), 'sp-|\.id', '');
-
-NairAbrahamCatalog = joinTables(cattbl, idtbl, 13, 3);
-
-clear cattbl idtbl
